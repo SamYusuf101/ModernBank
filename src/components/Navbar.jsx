dd6c7bdd850ea1b6863f9633458c898889da7481
Samyusuf101 <samyusuf101@gmail.com>
2022-09-21 13:45:15 -0700

    created hero component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,35 @@
 import React, { useState } from "react";
-import { close, logo, menu } from "../assets";
+import { logo } from "../assets";
 import { navLinks } from "../constants";
+import { close, menu } from "../assets";
 
 function Navbar() {
   const [toggle, setToggle] = useState(false);
   return (
-    <nav className="w-full flex py-6 justify-between items-center navbar">
-      <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
-      <ul className="list-none sm:flex hidden  justify-end items-center flex-1">
+    <nav className="w-full flex py-6 items-center navbar justify-between space-y-4">
+      <img src={logo} alt="Logo" className="w-[120px] h-[32px]" />
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
           <li
             key={nav.id}
             className={`font-poppins font-normal cursor-pointer text-[16px] text-white 
-            ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
+          ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
           >
-            <a href={`#${nav.id}`}>{nav.title}</a>
+            <a href={`#${nav.id}`}> {nav.title}</a>
           </li>
         ))}
       </ul>
-
-      <div className=" flex flex-1 sm:hidden justify-end items-center">
+      <div className="sm:hidden flex flex-1 justify-end items-center">
         <img
           src={toggle ? close : menu}
-          alt="menu"
-          className="w-[28px] h-[28px] object-contain"
+          alt="toggle"
+          className="w-[28px] h-[28px]"
           onClick={() => setToggle((prev) => !prev)}
         />
 
         <div
           className={`${toggle ? "flex" : "hidden"} p-6 bg-blue-500 absolute 
-          top-20 right-0 mx-4 my-2 min-w-[140px]
+          top-20 mt-5 right-0 mx-4 my-2 min-w-[140px]
         rounded-xl sidebar`}
         >
           <ul className="list-none flex flex-col justify-end items-center flex-1">
