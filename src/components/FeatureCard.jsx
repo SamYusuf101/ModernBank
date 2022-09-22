a1b63096e0f749e1fab38f1d9087d45170d4bb29
Samyusuf101 <samyusuf101@gmail.com>
2022-09-22 04:50:01 -0700

    created Business componenet

---
+++ src/components/FeatureCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const FeatureCard = ({ icon, title, index, content }) => {
+  <div>
+    <div>
+      <img src={icon} alt="icon" />
+      <p className="text-white">{title}</p>
+    </div>
+  </div>;
+};
+
+export default FeatureCard;
