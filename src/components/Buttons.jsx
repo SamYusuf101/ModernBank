17fd8fdd1c7ffae9a4446507642c6e6e8c1861b5
Samyusuf101 <samyusuf101@gmail.com>
2022-09-23 06:56:15 -0700

    created testimonials component

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -5,7 +5,7 @@ function Buttons({ styles }) {
     <button
       type="button"
       className={`px-6 py-4 bg-blue-gradient font-poppins font-medium
-    text-[18px] text-primary outline-none ${styles}`}
+    text-[18px] text-primary outline-none ${styles} rounded-[10px]`}
     >
       Get Started
     </button>
--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { card } from "../assets";
+import Buttons from "./Buttons";
+import styles, { layout } from "../styles";
 
-function cardDeal() {
-  return <div>cardDeal</div>;
-}
+const cardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className="sm:block hidden" />
+        in few easy steps
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Lorem Ips is simply dummy text of the printing and typesetting industry.
+        Lorem Ipsum has been the industry's standard dummy text ever since the
+        1500s, when an unknown printer took a galley of type and scrambled it to
+        make a type specimen book
+      </p>
+      <Buttons styles="mt-10" />
+    </div>
+    <div className={layout.sectionImg}>
+      <img src={card} alt="card" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+);
 
 export default cardDeal;
