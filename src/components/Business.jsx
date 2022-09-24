4b1739eb3ed8d318e6737e712eed749a8b99f375
Sam Yusuf <samyusuf101@gmail.com>
2022-09-24 22:03:37 +0100

    Update Business.jsx

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -21,7 +21,7 @@ function Business() {
           we'll handle the money.{" "}
         </h2>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-          With the right credit card you can improve your financial life by
+          With the right credit card we can help you improve your financial life by
           building credit, earning rewards and saving money. But with hundreds
           of credit cards in the market.
         </p>
