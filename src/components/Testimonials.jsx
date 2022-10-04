e65b13b0dbcf97b4a1f67eb40451a6929dc53ccf
Sam Yusuf <samyusuf101@gmail.com>
2022-10-04 23:22:47 +0100

    Update Testimonials.jsx

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+8import React from "react";
 import { feedback } from "../constants";
 import styles from "../styles";
 import FeedbackCard from "./FeedbackCard";
@@ -24,7 +24,7 @@ const Testimonials = () => (
       </h1>
       <div className="w-full md:mt-0 mt-6">
         <p className={`${styles.paragraph} text-left max-w-[450px]}`}>
-          Everything you need to accept card payments and grow your business
+          Everything you need, to accept card payments and grow your business
           anywhere on the planet.
         </p>
       </div>
